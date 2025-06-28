4e47fb4f05ace7635ba65d18ba6f9df42ae752e5
Leo Freitas <leofreeitas.lfm@gmail.com>
2025-06-28 15:00:42 -0300

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const users = [];
 function authenticateJWT(req, res, next) {
   const authHeader = req.header("Authorization");
   const token = authHeader?.split(" ")[1];
-  if (!token) return res.status(401).send("Token não fornecido.");
+  if (!token) return res.status(401).send("Acesso negado. Token não fornecido.");
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).send("Token inválido ou expirado.");
     req.user = user;
